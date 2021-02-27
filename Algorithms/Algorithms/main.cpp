dde6a1c3314723cb073d197f986149d8ee1fc339
hustle-dev <dlwoabsdk@gmail.com>
2021-02-27 23:18:41 +0900

    class2

--- Algorithms/Algorithms/main.cpp
+++ Algorithms/Algorithms/main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 int main(void)
 {
     ios::sync_with_stdio(0);
@@ -20,5 +19,27 @@ int main(void)
         v1.push_back(i+1);
     }
     
+    for(int i = 0; i<n; i++)
+    {
+        cout << v1[i] << '\n';
+    }
+
+    vector<int>::iterator iter = v1.begin();
     
+    
+    cout << *(iter+k-1) << '\n';
+    v1.erase(iter+k-1);
+    
+    cout << 
+    
+    
+//    v1.erase(v1.begin()+2);
+//
+//
+//    for(iter =v1.begin(); iter != v1.end(); iter++)
+//    {
+//        cout << *iter << '\n';
+//    }
+    
+    return 0;
 }
