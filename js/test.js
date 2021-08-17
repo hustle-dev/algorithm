bb2ae045623bf7a901f9ff6a7321e9e40e064a22
hustle-dev <dlwoabsdk@gmail.com>
2021-08-17 10:58:03 +0900

    conf: Create folder and mv file

--- js/test.js
+++ js/test.js
@@ -1,30 +1,11 @@
-function solution(n, times) {
-    let answer = Number.MAX_SAFE_INTEGER;
-    let lt = 1;
-    let rt = Math.max(...times) * n;
+function solution(N, number) {
+    let answer = 0;
+    let minValue = 0;
+    let dy = Array.from({length: number+1}, () => Number.MAX_SAFE_INTEGER);
 
-    function count(time) {
-        let cnt = 0;
-
-        for(let i = 0; i<times.length; i++) {
-            cnt += parseInt(time/times[i]);
-        }
-
-        return cnt;
-    }
-    
-    while(lt <= rt) {
-        let mid = parseInt((lt+rt)/2);
-        if(count(mid) >= n) {
-            answer = mid;
-            rt = mid-1;
-        }
-        else {
-            lt = mid+1;
-        }
-    }
+    dy[0] = 0;
 
     return answer;
 }
 
-console.log(solution(6, [7, 10]));
+console.log(solution(5, 12));
