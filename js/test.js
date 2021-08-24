4a220df5ec92d34fda61c27e43fcd568cc43e77b
hustle-dev <dlwoabsdk@gmail.com>
2021-08-24 11:06:11 +0900

    feat: algorithm review

--- js/test.js
+++ js/test.js
@@ -1,8 +1,6 @@
 function isPrime(num) {
     if (num === 1) return false;
-
     if (num === 2) return true;
-
     for(let i = 2; i<num; i++) {
         if(num % i === 0) {
             return false;
@@ -53,5 +51,3 @@ function solution(numbers) {
 
     return answer;
 }
-
-console.log(solution("17"));
