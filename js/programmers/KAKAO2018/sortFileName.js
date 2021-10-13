1085aacf0815207dc528f4f2b12c8f147b2f0924
hustle-dev <dlwoabsdk@gmail.com>
2021-10-13 09:35:01 +0900

    feat: code refactoring

--- js/programmers/KAKAO2018/sortFileName.js
+++ js/programmers/KAKAO2018/sortFileName.js
@@ -1,33 +1,17 @@
 const solution = files => {
-  let answer = [];
+  let answer;
+  const temp = [];
 
-  // head 기준 사전 정렬
-
-  const temp = Array.from({ length: files.length }, () => Array());
-
-  for (let i = 0; i < files.length; i++) {
-    temp[i].push(i);
-    files[i].replace(/[a-zA-Z-. ]+|[0-9]+/g, match => temp[i].push(match));
-    temp[i].push(files[i]);
-  }
+  files.forEach((file, i) => {
+    const HEAD = file.match(/[\D]+/g)[0];
+    const NUMBER = file.match(/[\d]+/g)[0];
+    temp.push([i, HEAD.toLowerCase(), Number(NUMBER), file]);
+  });
 
   temp.sort((a, b) =>
-    a[1].toLowerCase() > b[1].toLowerCase()
-      ? 1
-      : a[1].toLowerCase() < b[1].toLowerCase()
-      ? -1
-      : +a[2] > +b[2]
-      ? 1
-      : +a[2] < +b[2]
-      ? -1
-      : a[0] - b[0]
+    a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : a[2] > b[2] ? 1 : a[2] < b[2] ? -1 : a[0] - b[0]
   );
 
-  for (let i = 0; i < temp.length; i++) {
-    answer.push(temp[i].join(''));
-  }
-
   answer = temp.map(file => file[file.length - 1]);
-
   return answer;
 };
