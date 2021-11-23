00db4f24084e4afc77922e515d21d3cea07171d5
hustle-dev <dlwoabsdk@gmail.com>
2021-11-24 00:42:29 +0900

    feat: today algorithm solving

--- js/programmers/KAKAO2019/foodLive.js
+++ js/programmers/KAKAO2019/foodLive.js
@@ -1,3 +1,9 @@
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42891?language=javascript
+ * @param {number[]} food_times
+ * @param {number} k
+ * @returns
+ */
 function solution(food_times, k) {
   food_times.unshift(0);
   let sortFoodTimes = [...food_times];
