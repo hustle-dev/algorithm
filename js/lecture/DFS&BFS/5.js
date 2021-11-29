ddc45ed594ee03a934faac0c9592539e20722adc
hustle-dev <dlwoabsdk@gmail.com>
2021-11-29 21:39:48 +0900

    feat: add lecture DFS 5 problem

--- js/lecture/DFS&BFS/5.js
+++ js/lecture/DFS&BFS/5.js
@@ -1,61 +1,45 @@
-function solution(board) {
-  let answer = 0;
-  const queue = [];
-  const N = board.length;
-  const M = board[0].length;
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, -1, 0, 1];
+/**
+ * 최대 선호 음식
+ * @param {number[]} nums
+ * @param {number} D
+ * @param {number} K
+ * @returns
+ */
+const solution = (nums, D, K) => {
+  let answer = Number.MIN_SAFE_INTEGER;
+  const N = nums.length;
+
+  const pow = Array.from({ length: D + 1 }, () => 0);
+  const st = Array.from({ length: nums.length }, () => 0);
+  pow[1] = 1;
+
+  for (let i = 2; i <= D; i++) {
+    pow[i] = pow[i - 1] * 2;
+  }
 
   for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (board[i][j] === 1) {
-        queue.push([i, j]);
+    for (let j = 0; j < nums[i].length; j++) {
+      st[i] += pow[nums[i][j]];
+    }
+  }
+
+  function DFS(L, s, bits) {
+    if (L === K) {
+      let cnt = 0;
+      for (let j = 0; j < N; j++) {
+        if ((bits & st[j]) === st[j]) cnt++;
+      }
+      answer = Math.max(answer, cnt);
+    } else {
+      for (let i = s; i <= D; i++) {
+        DFS(L + 1, i + 1, bits + pow[i]);
       }
     }
   }
 
-  function BFS() {
-    let L = 1;
-    while (queue.length) {
-      let len = queue.length;
-      for (let i = 0; i < len; i++) {
-        const [curX, curY] = queue.shift();
-        for (let j = 0; j < 4; j++) {
-          const nx = curX + dy[j];
-          const ny = curY + dx[j];
-
-          if (nx >= 0 && ny >= 0 && nx < N && ny < M && board[nx][ny] === 0) {
-            board[nx][ny] = L;
-            queue.push([nx, ny]);
-          }
-        }
-      }
-      L++;
-    }
-  }
-
-  BFS();
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (board[i][j] === 0) {
-        return -1;
-      }
-
-      if (answer < board[i][j]) {
-        answer = board[i][j];
-      }
-    }
-  }
+  DFS(0, 1, 0);
 
   return answer;
-}
+};
 
-console.log(
-  solution([
-    [0, 0, -1, 0, 0, 0],
-    [0, 0, 1, 0, -1, 0],
-    [0, 0, -1, 0, 0, 0],
-    [0, 0, 0, 0, -1, 1],
-  ])
-);
+console.log(solution([[1], [2, 3], [3], [1, 2], [], [2, 1], [2, 3, 4], [3, 4]], 4, 3));
--- js/test.js
+++ js/test.js
@@ -1,18 +1,38 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
- */
-var containsNearbyDuplicate = function (nums, k) {
-  for (let lt = 0; lt < nums.length; lt++) {
-    for (let rt = lt + 1; rt <= lt + k; rt++) {
-      if (nums[lt] === nums[rt]) return true;
+const solution = (nums, D, K) => {
+  let answer = Number.MIN_SAFE_INTEGER;
+  const N = nums.length;
+
+  const pow = Array.from({ length: D + 1 }, () => 0);
+  const st = Array.from({ length: nums.length }, () => 0);
+  pow[1] = 1;
+
+  for (let i = 2; i <= D; i++) {
+    pow[i] = pow[i - 1] * 2;
+  }
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < nums[i].length; j++) {
+      st[i] += pow[nums[i][j]];
     }
   }
 
-  return false;
+  function DFS(L, s, bits) {
+    if (L === K) {
+      let cnt = 0;
+      for (let j = 0; j < N; j++) {
+        if ((bits & st[j]) === st[j]) cnt++;
+      }
+      answer = Math.max(answer, cnt);
+    } else {
+      for (let i = s; i <= D; i++) {
+        DFS(L + 1, i + 1, bits + pow[i]);
+      }
+    }
+  }
+
+  DFS(0, 1, 0);
+
+  return answer;
 };
 
-console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
-console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
-console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));
+console.log(solution([[1], [2, 3], [3], [1, 2], [], [2, 1], [2, 3, 4], [3, 4]], 4, 3));
