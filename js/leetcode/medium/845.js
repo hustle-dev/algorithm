33bcb2eb20cfc4bbf0978fa16cb2b145d0087a51
hustle-dev <dlwoabsdk@gmail.com>
2021-12-03 14:17:55 +0900

    feat: today algorithm solving

--- js/leetcode/medium/845.js
+++ js/leetcode/medium/845.js
@@ -5,40 +5,27 @@
  * @return {number}
  */
 var longestMountain = function (arr) {
-  const up = Array(arr.length).fill(0);
-  const down = Array(arr.length).fill(0);
-  const lengthArr = Array(arr.length).fill(0);
+  const N = arr.length;
+  const dp1 = Array(N).fill(1);
+  const dp2 = Array(N).fill(1);
+  const result = Array(N).fill(0);
 
-  let cnt = 1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i + 1]) {
-      up[i] = cnt;
-      cnt++;
-    } else {
-      up[i] = cnt;
-      cnt = 1;
+  for (let i = 1; i < N; i++) {
+    if (arr[i - 1] < arr[i]) {
+      dp1[i] = dp1[i - 1] + 1;
     }
   }
-  cnt = 1;
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] < arr[i - 1]) {
-      down[i] = cnt;
-      cnt++;
-    } else {
-      down[i] = cnt;
-      cnt = 1;
+
+  for (let i = N - 1; i >= 0; i--) {
+    if (arr[i] > arr[i + 1]) {
+      dp2[i] = dp2[i + 1] + 1;
     }
   }
-  if (Math.max(...down) === 1 || Math.max(...up) === 1) {
-    return 0;
+
+  for (let i = 0; i < N; i++) {
+    if (dp1[i] === 1 || dp2[i] === 1) continue;
+    result[i] = dp1[i] + dp2[i] - 1;
   }
-  for (let i = 0; i < arr.length; i++) {
-    if (up[i] === 1 || down[i] === 1) {
-      lengthArr[i] = -1;
-      continue;
-    }
-    lengthArr[i] = up[i] + down[i] - 1;
-  }
-  const answer = Math.max(...lengthArr);
-  return answer >= 3 ? answer : 0;
+
+  return Math.max(...result);
 };
