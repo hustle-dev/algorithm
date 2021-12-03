320a790926a8c7c490375bbc26db373027ab5f93
hustle-dev <dlwoabsdk@gmail.com>
2021-12-03 15:58:15 +0900

    feat: today algorithm solving

--- js/programmers/KAKAO2021/광고 삽입.js
+++ js/programmers/KAKAO2021/광고 삽입.js
@@ -51,13 +51,3 @@ const solution = (play_time, adv_time, logs) => {
 
   return convertToHHMMSS(index);
 };
-
-console.log(
-  solution('02:03:55', '00:14:15', [
-    '01:20:15-01:45:14',
-    '00:40:31-01:00:00',
-    '00:25:50-00:48:29',
-    '01:30:59-01:53:29',
-    '01:37:44-02:02:30',
-  ])
-);
