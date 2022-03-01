206965e092cb381c391d08f6a8570c9228bd62a5
hustle-dev <dlwoabsdk@gmail.com>
2022-03-01 20:31:58 +0900

    feat: today algorithm solving

--- js/lecture/그래프,DFS,BFS 응용/최대 선호 음식.js
+++ js/lecture/그래프,DFS,BFS 응용/최대 선호 음식.js
@@ -1,42 +1,33 @@
-/**
- * 최대 선호 음식
- * @param {number[]} nums
- * @param {number} D
- * @param {number} K
- * @returns
- */
-const solution = (nums, D, K) => {
-  let answer = Number.MIN_SAFE_INTEGER;
+const solution = (nums, d, k) => {
+  const pow = new Array(d + 1).fill(0);
   const N = nums.length;
+  const weightArr = new Array(N).fill(0);
 
-  const pow = Array.from({ length: D + 1 }, () => 0);
-  const st = Array.from({ length: nums.length }, () => 0);
   pow[1] = 1;
-
-  for (let i = 2; i <= D; i++) {
+  for (let i = 2; i <= d; i++) {
     pow[i] = pow[i - 1] * 2;
   }
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < nums[i].length; j++) {
-      st[i] += pow[nums[i][j]];
+      weightArr[i] += pow[nums[i][j]];
     }
   }
-  console.log(st);
 
-  function DFS(L, s, bits) {
-    if (L === K) {
-      let cnt = 0;
-      for (let j = 0; j < N; j++) {
-        if ((bits & st[j]) === st[j]) cnt++;
+  let answer = 0;
+  const DFS = (L, s, bits) => {
+    if (L === k) {
+      let sum = 0;
+      for (let i = 0; i < N; i++) {
+        if ((bits & weightArr[i]) === weightArr[i]) sum++;
       }
-      answer = Math.max(answer, cnt);
+      answer = Math.max(answer, sum);
     } else {
-      for (let i = s; i <= D; i++) {
+      for (let i = s; i <= d; i++) {
         DFS(L + 1, i + 1, bits + pow[i]);
       }
     }
-  }
+  };
 
   DFS(0, 1, 0);
 
