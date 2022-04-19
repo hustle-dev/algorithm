b7869f92ddf3ef1927f1b0479fe4e2e1d1345c6d
hustle-dev <dlwoabsdk@gmail.com>
2022-04-19 19:58:55 +0900

    feat: 연습

--- js/test.js
+++ js/test.js
@@ -1,51 +1,30 @@
-const solution = (tstring, variables) => {
-  const obj = {};
-  const N = variables.length;
-  const unf = new Array(N).fill(0);
-  const stringArr = tstring.split(' ');
+const mergeSort = arr => {
+  if (arr.length < 2) return arr;
 
-  for (const [variable, value] of variables) {
-    obj[variable] = value;
-  }
+  const mid = Math.floor(arr.length / 2);
 
-  for (let i = 0; i < stringArr.length; i++) {
-    const str = stringArr[i];
-    if (str === '{') {
-      console.log(str);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+  const resultArr = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] <= right[rightIndex]) {
+      resultArr.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArr.push(right[rightIndex]);
+      rightIndex++;
     }
   }
 
-  console.log(obj);
+  return resultArr.concat(left.slice(leftIndex), right.slice(rightIndex));
 };
 
-console.log(
-  solution('this is {template} {template} is {state}', [
-    ['template', 'string'],
-    ['state', 'changed'],
-  ]) // this is string string is changed
-);
-
-// console.log(solution('this is {template} {template} is {state}'), [
-//   ['template', 'string'],
-//   ['state', '{template}'],
-// ]); // this is string string is string
-
-// console.log(solution('this is {template} {template} is {state}'), [
-//   ['template', '{state}'],
-//   ['state', '{template}'],
-// ]); // this is {template} {template} is {state}
-
-// console.log(solution('this is {template} {template} is {state}'), [
-//   ['template', '{state}'],
-//   ['state', '{templates}'],
-// ]); // this is {templates} {templates} is {templates}
-
-// console.log(solution('{a} {b} {c} {d} {i}'), [
-//   ['b', '{c}'],
-//   ['a', '{b}'],
-//   ['e', '{f}'],
-//   ['h', 'i'],
-//   ['d', '{e}'],
-//   ['f', '{d}'],
-//   ['c', 'd'],
-// ]); // d d d {d} {i}
+console.log(mergeSort([5, 3, 8, 4, 9, 1, 6, 2, 7]));
+console.log(mergeSort([6, 3, 7, 2, 1, 8, 9, 4, 5, 10]));
